e5a1b59d04d22318c6a32db01f11ba8a840223a6
Abhinav <abhinavmishra.work@gmail.com>
2018-09-08 23:27:26 +0530

    setting up angular

---
+++ src/main/ui/nepalEMRUi/home/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import { TestComponent } from './test/test.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [TestComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
